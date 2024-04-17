84ac309bc3876abefa4bd0a39be76b763d84265a
Shahidkhan S <126046884+shahidkhan169@users.noreply.github.com>
2024-04-17 13:34:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43 MB (18.22%)

---
+++ 0050-powx-n/solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+class Solution {
+public double myPow(double x, int n) {
+        double p;
+        if(n>0)
+        {
+            return p=Math.pow(x,n);
+        }
+        else
+        {
+            return p=Math.pow(x,n);
+        }
+    
+    }
+};
