f887b4a6a5b35b9beaf499e354a0e998c3665d24
Shahidkhan S <126046884+shahidkhan169@users.noreply.github.com>
2024-08-03 13:15:27 +0000

    [LeetCode Sync] Runtime - 3 ms (82.41%), Memory - 44.4 MB (45.96%)

---
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        Arrays.sort(target);
+        Arrays.sort(arr);
+        int n=target.length;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]!=target[i])
+                return false;
+            
+        }
+        return true;
+    }
+}
