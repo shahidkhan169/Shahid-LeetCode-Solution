418b9a019866f9b3968d71c986ce567b2ba2eafe
Shahidkhan S <126046884+shahidkhan169@users.noreply.github.com>
2024-09-25 17:26:19 +0000

    [LeetCode Sync] Runtime - 3 ms (3.46%), Memory - 7.6 MB (58.51%)

--- 2083-three-divisors/solution.c
+++ 2083-three-divisors/solution.c
@@ -1,18 +1,9 @@
 bool isThree(int n) {
-    int num=sqrt(n);
-    if(num*num!=n)
-        return 0;
-    if(num<=1)
-        return 0;
-    if(num==2)
-        return 1;
-    if(num%2==0)
-        return 0;
-    int s=sqrt(num);
-    for(int i=3;i<=s;i+=2)
-    {
-        if(num%i==0)
-            return 0;
-    }
-    return 1;
+   int count=0;
+   for(int i=1;i<=n;i++)
+   {
+     if(n%i==0)
+        count++;
+   }
+   return count==3;
 }
