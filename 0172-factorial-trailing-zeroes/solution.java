0fc27c55aa6afe6eed49b1abc04bb55c2e79cc7d
Shahidkhan S <126046884+shahidkhan169@users.noreply.github.com>
2024-11-23 09:20:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.8 MB (40.16%)

---
+++ 0172-factorial-trailing-zeroes/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        int count=0;
+        int div=5;
+        while(n/div>0)
+        {
+            count+=n/div;
+            div*=5;
+        }
+        return count;
+     }
+}
