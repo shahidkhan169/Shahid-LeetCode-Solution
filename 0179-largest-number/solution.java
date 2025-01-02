970eaa0c4a9f139f45db910dce21586c0da8057b
Shahidkhan S <126046884+shahidkhan169@users.noreply.github.com>
2025-01-02 13:05:06 +0000

    [LeetCode Sync] Runtime - 6 ms (52.77%), Memory - 44.8 MB (6.67%)

--- 0179-largest-number/solution.java
+++ 0179-largest-number/solution.java
@@ -1,23 +1,15 @@
 class Solution {
     public String largestNumber(int[] nums) {
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            String str = Integer.toString(nums[i]);
-            list.add(str);
-        }
-        Collections.sort(list, new Comparator<String>() {
-            public int compare(String a, String b) {
-                String order1 = a + b;
-                String order2 = b + a;
-                return order2.compareTo(order1); } });
-        StringBuilder ans = new StringBuilder();
-        for (String s : list) {
-            ans.append(s);
-        }
-        if (ans.charAt(0) == '0') {
-            return "0";
-        }
-        return ans.toString();
+        int n=nums.length;
+        String[] str=new String[n];
+        for(int i=0;i<n;i++)
+            str[i]=String.valueOf(nums[i]);
+        Arrays.sort(str,(a,b)->(b+a).compareTo(a+b));
+        if(str[0].equals("0"))
+            return "0"; 
+        String ans="";
+        for(String i:str)
+            ans+=i;
+        return ans;
     }
 }
-
