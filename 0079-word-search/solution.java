aa988c0156b620183064b6c78049c79d422fd3cb
Shahidkhan S <126046884+shahidkhan169@users.noreply.github.com>
2025-01-08 14:23:28 +0000

    [LeetCode Sync] Runtime - 154 ms (41.68%), Memory - 41.6 MB (60.04%)

---
+++ 0079-word-search/solution.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean check(int i, int j, int r, int c, int ind, char[][] board, String word, int[][] visited) {
+        if (ind == word.length())
+            return true;
+        if (i < 0 || i >= r || j < 0 || j >= c) {
+            return false;
+        }
+        if (board[i][j] != word.charAt(ind))
+            return false;
+        if (visited[i][j] == 1)
+            return false;
+
+        visited[i][j] = 1;
+        if (check(i + 1, j, r, c, ind + 1, board, word, visited) || check(i - 1, j, r, c, ind + 1, board, word, visited)
+                || check(i, j + 1, r, c, ind + 1, board, word, visited)
+                || check(i, j - 1, r, c, ind + 1, board, word, visited)) {
+            return true;
+        }
+        visited[i][j] = 0;
+        return false;
+    }
+
+    public boolean exist(char[][] board, String word) {
+        int r = board.length;
+        int c = board[0].length;
+        int[][] visited = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                char ch = board[i][j];
+                if (ch == word.charAt(0)) {
+                    boolean a = check(i, j, r, c, 0, board, word, visited);
+                    if (a == true) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
