a1adea2e08bf67aee4757a2978a999557d5ebc9f
Shahidkhan S <126046884+shahidkhan169@users.noreply.github.com>
2025-01-21 06:16:00 +0000

    [LeetCode Sync] Runtime - 3 ms (63.75%), Memory - 7.8 MB (47.15%)

--- 1013-fibonacci-number/solution.c
+++ 1013-fibonacci-number/solution.c
@@ -1,22 +1,18 @@
-int fib(int n){
-    if(n==0)
-        return 0;
-    if(n==1)
-        return 1;
-    else
+int solve(int n,int *dp)
+{
+    if(n==0 || n==1)
     {
-        int c,i;
-        i=0;
-        int a=0,b=1;
-        while(i+1<n)
-        {
-            c=a+b;
-            a=b;
-            b=c;
-            i++;
-        }
-        return c;
+        dp[n]=n;
+        return n;
     }
-    return -1;
-        
+    if(dp[n]!=-1)
+        return dp[n];
+    return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+}
+int fib(int n){
+    int dp[n+1];
+    for(int i=0;i<=n;i++)
+        dp[i]=-1;
+    solve(n,dp);
+    return dp[n];
 }
